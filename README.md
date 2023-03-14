6b88277fa28c90ebe5325c5ad349b81402262733
Tobias Preuss <tobias.preuss@googlemail.com>
2023-03-14 22:17:51 +0100

    Initial import.

---
+++ README.md
@@ -0,0 +1,39 @@
+# File download
+
+A file download Python3 script which checks file size,
+HTTP status codes and takes HTTP ETag into account.
+
+
+## Usage
+
+``` bash
+python file_download.py <url> <file_path>
+```
+
+## Output files
+
+- the downloaded file as `<file_path>`
+- the ETag as `<file_path>.etag`
+
+
+## License
+
+```
+File download - A file download Python3 script which checks file size,
+HTTP status codes and takes HTTP ETag into account.
+
+Copyright (C) 2023 Tobias Preuss
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+```
---
+++ file_download.py
@@ -0,0 +1,63 @@
+import urllib.request
+import os
+import sys
+from http import HTTPStatus
+from urllib.error import HTTPError
+
+if len(sys.argv) != 3:
+    print(f'Usage: python {sys.argv[0]} <url> <file_path>')
+    sys.exit(1)
+
+url = sys.argv[1]
+file_path = sys.argv[2]
+
+directory_path = os.path.dirname(file_path)
+if not os.path.exists(directory_path):
+    print(f'Error: directory "{directory_path}" does not exist')
+    sys.exit(1)
+
+if os.path.exists(file_path + '.etag'):
+    with open(file_path + '.etag', 'r') as f:
+        etag = f.read().strip()
+        headers = {'If-None-Match': etag}
+else:
+    headers = {}
+
+req = urllib.request.Request(url, headers=headers)
+try:
+    response = urllib.request.urlopen(req)
+except HTTPError as e:
+    if e.code == HTTPStatus.NOT_MODIFIED:
+        print('File has not been modified')
+        sys.exit(0)
+    else:
+        print(f'Download failed with HTTP {e.code}')
+        sys.exit(1)
+
+if response.status == HTTPStatus.OK:
+    content_length = response.headers.get('Content-Length')
+    if content_length is not None:
+        file_size = int(content_length)
+        print(f'File size is {file_size} bytes')
+    else:
+        print('Could not determine file size')
+
+    with open(file_path, 'wb') as f:
+        f.write(response.read())
+    print(f'Downloaded file from "{url}" to "{file_path}" successfully')
+
+    downloaded_file_size = os.path.getsize(file_path)
+    if downloaded_file_size == file_size:
+        print('File size matches expected size')
+    else:
+        print(f'Error: downloaded file size ({downloaded_file_size} bytes) does not match expected size ({file_size} bytes)')
+
+    etag = response.headers.get('ETag')
+    if etag is not None:
+        with open(file_path + '.etag', 'w') as f:
+            f.write(etag)
+        print(f'Saved ETag {etag} to file {file_path}.etag')
+
+else:
+    print(f'Download failed with HTTP {response.status}')
+    sys.exit(1)
